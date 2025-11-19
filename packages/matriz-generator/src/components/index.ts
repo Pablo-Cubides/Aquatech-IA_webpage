0a09c96152ab693566495c6a664a4d755775eb19
Pablo-Cubides <pacubidesg@unal.edu.co>
2025-11-19 10:55:56 -0500

    chore: remove unused @ia-next/matriz-generator package
    - Remove packages/matriz-generator (standalone package never used)
    - All matriz functionality already integrated in apps/web/src/components
    - Remove unused dependency from apps/web/package.json
    - App uses direct component imports from @/components, not package imports
    
    This package was an incomplete attempt to create a reusable NPM package.
    The functionality was directly integrated into the web app instead.

--- packages/matriz-generator/src/components/index.ts
+++
@@ -1,8 +0,0 @@
-// Componentes principales de la herramienta
-export { default as HeroTabs } from "./HeroTabs";
-export { default as LeopoldMatrix } from "./LeopoldMatrix";
-export { default as LeopoldGrid } from "./LeopoldGrid";
-export { default as ConesaForm } from "./ConesaForm";
-export { default as BattelleTable } from "./BattelleTable";
-export { default as ExportButtons } from "./ExportButtons";
-export { default as MatrixInfoCard } from "./MatrixInfoCard";
